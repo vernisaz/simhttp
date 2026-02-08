9f063cf7d908fb6d7c9b08b329c5c2e7380a50c0
D R <metricstream@gmail.com>
2026-02-07 20:04:24 -0800

    applied formatting

--- log.rs
+++ log.rs
@@ -1,17 +1,17 @@
-use std::{
-    fs::{self,File},
-    time::{SystemTime,UNIX_EPOCH},
-    path::{/*MAIN_SEPARATOR_STR,*/PathBuf},
-    io::{Seek},
-    fmt::Display,
-};
 use crate::io;
+use std::{
+    fmt::Display,
+    fs::{self, File},
+    io::Seek,
+    path::{/*MAIN_SEPARATOR_STR,*/ PathBuf},
+    time::{SystemTime, UNIX_EPOCH},
+};
 
 static MAX_LINES: u32 = 10_1000;
 
 #[derive(Debug, Clone, PartialEq, Default)]
 pub enum Level {
-     #[default]
+    #[default]
     All,
     Trace,
     Warning,
@@ -46,9 +46,12 @@ impl From<u32> for Level {
     }
 }
 
-impl <'a>SimLogger<'a> {
-    pub fn new(level: Level, output: impl std::io::Write + Sync + Send +'static) -> Self {
-        Self { level, output: Box::new(output) }
+impl<'a> SimLogger<'a> {
+    pub fn new(level: Level, output: impl std::io::Write + Sync + Send + 'static) -> Self {
+        Self {
+            level,
+            output: Box::new(output),
+        }
     }
     pub fn log(&mut self, level: Level, message: &str) {
         if self.level.clone() as u32 <= level as u32 {
@@ -77,50 +80,59 @@ impl <'a>SimLogger<'a> {
 
 impl LogFile {
     pub fn new() -> Self {
-        let created = SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_millis() as u64;
-        let name = format!{"simhttp-{}", created};
+        let created = SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .unwrap()
+            .as_millis() as u64;
+        let name = format! {"simhttp-{}", created};
         let mut path = PathBuf::from(".");
         path.push(name.clone());
         path.set_extension("log");
         let file = File::create(path).expect("can't create log");
-    
-        LogFile { currnet_line: 0,
+
+        LogFile {
+            currnet_line: 0,
             current_chunk: 0,
             path: None,
             name,
             file,
         }
     }
-    
+
     pub fn from(path: impl Into<String>, name: &impl AsRef<str>) -> Self {
-        let created = SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_millis() as u64;
-        //let args = format!("{vec![Box::new(created}"))]; // created 
-        let name:String = name.as_ref().to_string();
-        let name = simweb::interpolate(&name,&vec![Box::new(&created as &dyn Display)]);
-        let path:String=path.into();
+        let created = SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .unwrap()
+            .as_millis() as u64;
+        //let args = format!("{vec![Box::new(created}"))]; // created
+        let name: String = name.as_ref().to_string();
+        let name = simweb::interpolate(&name, &vec![Box::new(&created as &dyn Display)]);
+        let path: String = path.into();
         let mut log_path = PathBuf::from(&path);
         log_path.push(name.clone());
         log_path.set_extension("log");
-        let file = File::create(&log_path).unwrap_or_else(|e| panic!("can't create log {log_path:?}/{e:?}"));
-    
-        LogFile { currnet_line: 0,
+        let file = File::create(&log_path)
+            .unwrap_or_else(|e| panic!("can't create log {log_path:?}/{e:?}"));
+
+        LogFile {
+            currnet_line: 0,
             current_chunk: 0,
             path: Some(path),
             name,
             file,
         }
     }
-    
-    pub fn roll (&mut self) {
+
+    pub fn roll(&mut self) {
         self.current_chunk += 1;
         let mut path = match &self.path {
             None => PathBuf::from("."),
-            Some (path) => PathBuf::from(path)
+            Some(path) => PathBuf::from(path),
         };
         path.push(self.name.clone());
         path.set_extension("log");
         let mut copy_path = path.clone();
-        copy_path.set_extension( format!{"log.{:05}", self.current_chunk});
+        copy_path.set_extension(format! {"log.{:05}", self.current_chunk});
 
         if fs::copy(path, copy_path).is_ok() && self.file.rewind().is_ok() {
             let _ = self.file.set_len(0);
@@ -131,14 +143,15 @@ impl LogFile {
 impl std::io::Write for LogFile {
     fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
         self.file.write_all(buf)?;
-        if buf[buf.len()-1] == b'\n' { // add count of all \n in the string
+        if buf[buf.len() - 1] == b'\n' {
+            // add count of all \n in the string
             self.currnet_line += 1;
             if self.currnet_line > MAX_LINES {
                 self.roll();
                 self.currnet_line = 0;
             }
         }
-        
+
         Ok(buf.len())
     }
 
